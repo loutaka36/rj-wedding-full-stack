131c135d35f1ea7524d33c1ae46f9bb671f8b395
rtakahashi <lou.takahashi@gmail.com>
2020-02-02 16:25:12 -0600

    modified story on home page

--- client/src/Accommodations.js
+++ client/src/Accommodations.js
@@ -9,16 +9,19 @@ const Accommodations = () => {
       </div>
       <div className="accommodations-hotel">
         <p>
-        We have a hotel block at Hyatt Regency Lisle. Yes, this is the same location as the reception, so if you book a hotel room here, you can just enjoy the reception and then go upstairs to your hotel room to sleep!<br/><br/>
+          We have a hotel block at <b>Hyatt Regency Lisle</b>. Yes, this is the same location as the reception, so if you book a hotel room here, you can just enjoy the reception and then go upstairs to your hotel room to sleep!<br/><br/>
 
-        Single and double rooms are $110. Triple rooms are $120. Breakfast is included.<br/><br/>
+          Single and double rooms are $110. Triple rooms are $120. Breakfast is included.<br/><br/>
 
-        Please book your rooms <a href="#" target="_blank" rel="noopener noreferrer">here</a>.<br/><br/>
-
-        If you plan to explore the area, Hyatt Regency Lisle offers a free shuttle within a 5-mile radius of the hotel.<br/><br/>
-
-        Please reach out to us if you have any questions!
+          Please book your rooms <a href="#" target="_blank" rel="noopener noreferrer">here</a>.
         </p>
+        <img className="accommodations-flower "src={require('./engagement-pics/carnations.png')} />
+        <p>
+          If you plan to explore the area, Hyatt Regency Lisle offers a free shuttle within a 5-mile radius of the hotel.<br/><br/>
+
+          Please reach out to us if you have any questions!
+        </p>
+        <img className="accommodations-hotel-image" src={require('./engagement-pics/hyatt.webp')} />
       </div>
     </div>
   );
--- client/src/Home.js
+++ client/src/Home.js
@@ -17,15 +17,15 @@ const Home = () => {
       <div className="home-story">
         <div className="home-story-title">Our Story</div>
         <p>
-          We met in Cleveland, Ohio as students at Case Western Reserve University. Both of us were part of InterVarsity Christian Fellowship, which is where we got to know each other.<br/><br/>
+          We met in Cleveland, Ohio as students at Case Western Reserve University.<br/><br/>
 
-          2014 - Joyce's first memory of Lou was at one of her first InterVarsity large groups, where Lou shared his testimony of converting to Christianity. Joyce admired Lou for his faith, and was star-struck immediately. However, she thought he was out of her league because he was a good-looking athlete, and she was a small couch potato freshman. So she just admired him from afar for the next few years and did not think too much about him since there was no hope anyway.<br/><br/>
+          2014 - Joyce's first memory of Lou was at one of her first InterVarsity large groups, where Joyce was star-struck immediately. However, she thought he was out of her league because she was a small potato freshman. So she just admired him from afar for the next few years and did not think too much about him since there was no hope anyway.<br/><br/>
 
-          2016 - Lou started becoming interested in Joyce when he was a junior and Joyce was a sophomore. He had been driving students to monthly regional InterVarsity meetings, and he found Joyce to be a happy conversationalist in the car. Once, he even missed a highway exit because he was so distracted with talking to her.<br/><br/>
+          2016 - Lou started becoming interested in Joyce when he started driving students to monthly regional InterVarsity meetings. He found Joyce to be a happy conversationalist in the car. Once, he even missed a highway exit because he was so distracted with talking to her.<br/><br/>
 
-          2017 - When Lou became a senior, he knew time was running short. He spent quite some time talking to Joyce at InterVarsity events the first month of senior year. However, Joyce was quite oblivious and thought he was just being friendly because she had zero hope that Lou would be interested in her. Little did she know that Lou was observing her schedule and trying to "coincidentally" appear in places that she was.<br/><br/>
+          2017 - When Lou became a senior, he knew time was running short. He spent quite some time talking to Joyce at InterVarsity events the first month of senior year. However, Joyce was quite oblivious and thought he was just being friendly because she had zero hope that Lou would be interested in her.<br/><br/>
 
-          One day, Lou "coincidentally" was walking in the same direction as Joyce. As they walked and talked, Lou stopped Joyce in the middle of the street and nervously and eloquently asked her to go on a date. Joyce replied, "uh, sure...?" The fangirl in Joyce had become very level-headed about relationships. However, after a few months of getting to know each other, Joyce realized that she had admired the right person all these years and became totally smitten, and Lou was convinced that he had found his better half.<br/><br/>
+          One day, Lou was "coincidentally" walking in the same direction as Joyce. As they walked and talked, Lou stopped Joyce in the middle of the street and nervously asked her to go on a date. Joyce replied, "uh, sure...?" The fangirl in Joyce had become very level-headed about relationships. However, after a few months of getting to know each other, Joyce realized that she had admired the right person all these years and became totally smitten, and Lou was convinced that he had found his better half.<br/><br/>
 
           Now Joyce is finishing her graduate program, and Lou is changing careers. We have many unknowns ahead, but we are excited to embark on our journey together.
         </p>
