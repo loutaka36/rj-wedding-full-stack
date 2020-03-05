08b4105bb04d588d890de5194328b718f06ea5b1
rtakahashi <lou.takahashi@gmail.com>
2020-03-05 17:07:18 -0600

    fix

--- client/src/RSVP.js
+++ client/src/RSVP.js
@@ -32,7 +32,7 @@ class RSVP extends React.Component {
         <div className="rsvp">
           <div className="rsvp-top">
             <div className="rsvp-top_title">RSVP</div>
-            <div className="rsvp-top_text">{`Hi, ${this.props.guest.guestData.firstName}! Who will be attending?`}</div>
+            {/* <div className="rsvp-top_text">{`Hi, ${this.props.guest.guestData.firstName}! Who will be attending?`}</div> */}
           </div>
           <RSVPSelect />
         </div>
