777500cf7dce0950499897024c5358faec2f3f6e
rtakahashi <lou.takahashi@gmail.com>
2021-04-10 16:15:17 -0400

    Changed db connection dialect options.

--- server/db/db.js
+++ server/db/db.js
@@ -2,7 +2,13 @@ const Sequelize = require('sequelize');
 const db = new Sequelize(
   process.env.DATABASE_URL ||
   'postgres://localhost:5432/wedding', {
-    logging: false
+    logging: false,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+          rejectUnauthorized: false
+      }
+    }
   }
 );
 
